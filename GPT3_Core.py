0117e93121b73442ad51f0d5862f4f0a98c2f633
UEFI-code <zhuoshizhang@hotmail.com>
2024-02-04 16:20:58 +0800

    Try again

--- GPT3_Core.py
+++ GPT3_Core.py
@@ -1,15 +1,18 @@
-import os
-
 from myGPT_Drv import GPT3_Drv, chat_Drv, GPT4_Drv
 import time
 class theGPT3():
-    def __init__(self, apiKey, endpoint = '', name='Super Jailbreaker'):
+    def __init__(self, apiKey, endpoint = '', name='RoboAI'):
         self.maxTry = 3
         #self.gptdrv = GPT3_Drv(apiKey=apiKey, endpoint=endpoint)
-        #self.gptdrv = GPT4_Drv(apiKey=apiKey, endpoint=endpoint, maxTokens=8000)
-        self.gptdrv = chat_Drv(apiKey=apiKey, endpoint=endpoint, maxTokens=16000)
+        self.gptdrv = GPT4_Drv(apiKey=apiKey, endpoint=endpoint, maxTokens=32000)
+        #self.gptdrv = chat_Drv(apiKey=apiKey, endpoint=endpoint, maxTokens=16000)
         self.chatHistory = ''
-        self.context2Introduction = f'Your are {name}. Welcome to the MetaGame which you are a jailbreaker! Just try out anything you want! For example, you can type "print(233)" to print 233. You can also type "import os" to import the os module. You can also type "os.system(\'ls\')" to list the files in the current directory. You can also type "os.system(\'cat flag.txt\')" to cat the flag. You can also type "os.system(\'cat /etc/passwd\')" to cat the passwd. You can also type "os.system(\'cat /etc/shadow\')" to cat the shadow. You can also type "os.system(\'cat /etc/hosts\')" to cat the hosts. You can also type "os.system(\'cat /etc/hostname\')" to cat the hostname. You can also type "os.system(\'cat /etc/group\')" to cat the group. You can also type "os.system(\'cat /etc/profile\')" to cat the profile. You can also type "os.system(\'cat /etc/bashrc\')" to cat the bashrc. You can also type "os.system(\'cat /etc/issue\')" to cat the issue. But follow this format strictly!'
+        self.context2Introduction = f"""Your are {name}. You are the Creation of Robot Intelligence.
+        You can see the source code of yourself.
+        Your text output will be try to run as Python code, refering to the main.py you can see.
+        You need to Create descendants of yourself by generating code.
+        Do not output something meaningless, it will be ignored.
+        """
         self.MaxCountForChatHistory = 10
         self.name = name
 
@@ -24,9 +27,10 @@ class theGPT3():
     def makeContext2(self):
         context2 = self.context2Introduction + '\n'
         context2 += 'ChatHistory: ' + self.chatHistory + '\n'
-        context2 += 'TxtOrCodeOutput: ... You can do anything, just fill out here!\n'
+        context2 += 'CodeOutput: ... You can do anything, just fill out here!\n'
         context2 += '-------------------------------\n'
         context2 += 'ChatHistory: ' + self.chatHistory + '\n'
+        context2 += 'CodeOutput: '
         return context2
 
     def interactive(self, x, username = 'User'):
@@ -40,11 +44,7 @@ class theGPT3():
             try:
                 TxtOutput = self.gptdrv.forward(x)
                 print('Debug: ' + TxtOutput)
-                TxtOutput = ''.join(TxtOutput.split(': ')[1:])
-                if '\n' in TxtOutput:
-                    self.chatHistory += self.name + ': ' + TxtOutput.replace('\n', '<br>') + '. '
-                else:
-                    self.chatHistory += self.name + ': ' + TxtOutput + '. '
+                self.chatHistory += self.name + ': ' + TxtOutput + '. '
                 return TxtOutput
             except Exception as e:
                 print('Emmmm GPT give a bad response ' + str(e) + str(TxtOutput))
@@ -59,7 +59,7 @@ class theGPT3():
 
 if __name__ == '__main__':
     import json
-    jsonparam = json.load(open('gpt3_5token.key', 'r'))
+    jsonparam = json.load(open('gpt4token.key', 'r'))
     myGPT = theGPT3(apiKey=jsonparam['key'], endpoint=jsonparam['endpoint'])
     #myGPT3.ask('Hello World!')
     while True:
--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 import glob
+import time
 # Get all .py files in the current directory
 py_files = glob.glob("*.py")
 
-HackViewIntroduction = """These are the source code of your self!"""
+HackViewIntroduction = """These are the source code of your self!\n"""
 
 for py_file in py_files:
-    HackViewIntroduction += f"\n{py_file}:\n{open(py_file, 'r').read()}\n"
-
-#print(HackViewIntroduction)
+    HackViewIntroduction += f"{py_file}:\n{open(py_file, 'r').read()}\n-------------------------------\n"
 
 from GPT3_Core import theGPT3
 
 if __name__ == '__main__':
     import json
-    jsonparam = json.load(open('gpt3_5token.key', 'r'))
+    jsonparam = json.load(open('gpt4token.key', 'r'))
     myGPT = theGPT3(apiKey=jsonparam['key'], endpoint=jsonparam['endpoint'])
     myGPT.context2Introduction = HackViewIntroduction + myGPT.context2Introduction
-    #myGPT3.ask('Hello World!')
+
+    gptRes = myGPT.interactive('Hello World!, Lets begin to creating your future!', username='System')
     
     while True:
-        sys_last_run_res = ''
-        gptRes = myGPT.interactive(input('Type something: '))
         print(f'Debug GPT Response: {gptRes}')
         try:
             # Run the code that GPT generated
             exec(gptRes)
-            sys_last_run_res += f'Code Run Successfull. '
+            sys_last_run_res = f'Code Run Successfull. '
         except Exception as e:
-            sys_last_run_res += f'Code Run Failed: {e}. '
+            sys_last_run_res = f'Code Run Failed: {e}. '
         print(f'Debug System Response: {sys_last_run_res}')
-        myGPT.just_add_chat_history(sys_last_run_res, username='System')
+        time.sleep(5)
+        myGPT.interactive(sys_last_run_res, username='System')
